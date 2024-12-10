7892532c22956b165abc2d201fd8976ccd399d78
Rinai-R <2024211885@stu.cqupt.edu.cn>
2024-12-10 21:13:03 +0800

    Update main.go

--- PurChase/main.go
+++ PurChase/main.go
@@ -15,8 +15,8 @@ func main() {
 	var rdb *redis.Client
 	ctx = context.Background()
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     "192.168.195.128:6379",
-		Password: "~Cy710822",
+		Addr:     "ip",
+		Password: "password",
 		DB:       0,
 	})
 	for i := 0; i <= 10; i++ {

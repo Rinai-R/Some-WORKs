c7bc6026cd1e8dd5dbe7596b5b2a0531f452b4d6
Rinai_ <2024211885@stu.cqupt.edu.cn>
2025-02-09 19:44:34 +0800

    kitex和thrift小demo，测试成功

---
+++ 2025/02February/20250209/kitex/App/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "Golang/2025/02February/20250209/kitex/App/router"
+
+func main() {
+	router.InitRouter()
+}
--- 2025/02February/20250209/kitex/server/user/main.go
+++ 2025/02February/20250209/kitex/server/user/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	user "Golang/2025/02February/20250209/kitex/kitex_gen/user/user"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
 	"log"
+	"net"
 )
 
 func main() {
-	svr := user.NewServer(new(UserImpl))
 
-	err := svr.Run()
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:10001")
+	if err != nil {
+		panic(err)
+	}
+	svr := user.NewServer(&UserImpl{},
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "user"}),
+		server.WithServiceAddr(addr), // address
+	)
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())

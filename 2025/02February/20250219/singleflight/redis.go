139e2bc0d7f0f1e2be37f6e367d47205a97671f0
Rinai_ <2024211885@stu.cqupt.edu.cn>
2025-02-19 15:02:46 +0800

    setnx,防缓存穿透

--- 2025/02February/20250219/singleflight/redis.go
+++ 2025/02February/20250219/singleflight/redis.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"golang.org/x/sync/singleflight"
+	"math/rand"
 	"strconv"
 	"sync"
+	"time"
 )
 
 type service struct {
@@ -33,7 +35,7 @@ func init() {
 	})
 }
 
-func (s *service) handleRequest(ctx context.Context, request *Request) (*Response, error) {
+func (s *service) handleRequest_Group(ctx context.Context, request *Request) (*Response, error) {
 	str, _ := json.Marshal(request)
 	v, err, _ := s.requestGroup.Do(string(str), func() (interface{}, error) {
 		result, err := rdb.HGetAll(ctx, "user:"+strconv.FormatInt(request.user_id, 10)).Result()
@@ -51,6 +53,36 @@ func (s *service) handleRequest(ctx context.Context, request *Request) (*Respons
 	return v.(*Response), nil
 }
 
+func (s *service) handleRequest(ctx context.Context, request *Request) (*Response, error) {
+	cacheKey := fmt.Sprintf("user:%d", request.user_id)
+	res, err := rdb.HGetAll(ctx, cacheKey).Result()
+	if len(res) == 0 {
+		LockKey := "locked:" + cacheKey
+		locked, _ := rdb.SetNX(ctx, LockKey, "locked", time.Second*10).Result()
+		fmt.Println("locked:", locked)
+		if locked {
+			fmt.Println("redis setnx-----------------------------------------------")
+			//todo
+			//访问数据库，将数据存入缓存
+			rdb.Del(ctx, LockKey)
+			return &Response{
+				username: "sql查询",
+				password: "sql查询",
+			}, nil
+		} else {
+			t := rand.Int() % 100
+			time.Sleep(time.Millisecond * time.Duration(t))
+			return s.handleRequest(ctx, request)
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &Response{
+		username: res["name"],
+		password: res["password"],
+	}, nil
+}
 func main() {
 	var ctx = context.Background()
 	wg := &sync.WaitGroup{}
@@ -62,7 +94,7 @@ func main() {
 			defer wg.Done() // 确保每个 goroutine 都调用 wg.Done()
 
 			response, err := s.handleRequest(ctx, &Request{
-				user_id: 1,
+				user_id: 2,
 			})
 			if err != nil {
 				// 打印错误信息，并返回

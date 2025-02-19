1370c1d64ae6e7328ad7a025cb254e3688ebc75b
Rinai_ <2024211885@stu.cqupt.edu.cn>
2025-02-19 13:53:10 +0800

    利用singleflight.Group解决缓存击穿问题

---
+++ 2025/02February/20250219/singleflight/redis.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"golang.org/x/sync/singleflight"
+	"strconv"
+	"sync"
+)
+
+type service struct {
+	requestGroup singleflight.Group
+}
+
+type Request struct {
+	user_id int64
+}
+
+type Response struct {
+	username string
+	password string
+}
+
+var rdb *redis.Client
+
+func init() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "123456",
+		DB:       0,
+	})
+}
+
+func (s *service) handleRequest(ctx context.Context, request *Request) (*Response, error) {
+	str, _ := json.Marshal(request)
+	v, err, _ := s.requestGroup.Do(string(str), func() (interface{}, error) {
+		result, err := rdb.HGetAll(ctx, "user:"+strconv.FormatInt(request.user_id, 10)).Result()
+		if err != nil {
+			return nil, err
+		}
+		return &Response{
+			username: result["name"],
+			password: result["password"],
+		}, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return v.(*Response), nil
+}
+
+func main() {
+	var ctx = context.Background()
+	wg := &sync.WaitGroup{}
+	n := 1000
+	wg.Add(n)
+	s := &service{requestGroup: singleflight.Group{}}
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done() // 确保每个 goroutine 都调用 wg.Done()
+
+			response, err := s.handleRequest(ctx, &Request{
+				user_id: 1,
+			})
+			if err != nil {
+				// 打印错误信息，并返回
+				fmt.Println("Error:", err)
+				return
+			}
+			// 如果没有错误，打印 response
+			fmt.Println(*response)
+		}()
+	}
+	wg.Wait() // 等待所有 goroutine 完成
+}

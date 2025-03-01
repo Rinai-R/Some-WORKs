0d08c738fbc68505b42aa4f5e9fbac3e14638889
Rinai_ <2024211885@stu.cqupt.edu.cn>
2025-03-01 15:50:02 +0800

    协程池优化，处理了在通道关闭后的panic

--- 2025/03March/20250301/Goroutine_pool/main.go
+++ 2025/03March/20250301/Goroutine_pool/main.go
@@ -11,6 +11,10 @@ type GoroutinePool struct {
 	Count sync.WaitGroup
 
 	Tasks chan func()
+
+	closed bool
+
+	mutex sync.Mutex
 }
 
 func NewGoroutinePool(maxGoroutineNum int) *GoroutinePool {
@@ -18,10 +22,13 @@ func NewGoroutinePool(maxGoroutineNum int) *GoroutinePool {
 		MaxGoroutineNum: maxGoroutineNum,
 		Count:           sync.WaitGroup{},
 		Tasks:           make(chan func(), maxGoroutineNum),
+		closed:          true,
+		mutex:           sync.Mutex{},
 	}
 }
 
 func (pool *GoroutinePool) Start() {
+	pool.closed = false
 	for i := 0; i < pool.MaxGoroutineNum; i++ {
 		go pool.Work()
 	}
@@ -29,17 +36,33 @@ func (pool *GoroutinePool) Start() {
 
 func (pool *GoroutinePool) Work() {
 	for task := range pool.Tasks {
-		task()
-		pool.Count.Done()
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println("goroutine panic:", r)
+				}
+				pool.Count.Done()
+			}()
+			task()
+		}()
 	}
 }
 
 func (pool *GoroutinePool) SubmitTask(task func()) {
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+	if pool.closed {
+		fmt.Println("goroutine pool closed")
+		return
+	}
 	pool.Count.Add(1)
 	pool.Tasks <- task
 }
 
 func (pool *GoroutinePool) Stop() {
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
+	pool.closed = true
 	close(pool.Tasks)
 }
 
@@ -48,14 +71,15 @@ func (pool *GoroutinePool) Wait() {
 }
 
 func main() {
-	pool := NewGoroutinePool(500)
+	pool := NewGoroutinePool(100)
 	pool.Start()
+
 	for i := 1; i <= 1000; i++ {
 		x := i
-		pool.SubmitTask(func() {
+		go pool.SubmitTask(func() {
 			fmt.Println("这是第", x, "次执行任务！")
 		})
 	}
-	defer pool.Stop()
 	defer pool.Wait()
+	defer pool.Stop()
 }

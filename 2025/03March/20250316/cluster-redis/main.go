6040ae4af8afaaa04be43a8a9ac580ba8a3fce9d
2024211885-Rinai <2024211885@stu.cqupt.edu.cn>
2025-03-16 21:51:15 +0800

    feat:redis分片集群的客户端

---
+++ 2025/03March/20250316/cluster-redis/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	ctx := context.Background()
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{
+			"localhost:7001",
+			"localhost:7002",
+			"localhost:7003",
+			"localhost:7004",
+			"localhost:7005",
+			"localhost:7006",
+		},
+	})
+	if err := rdb.Set(ctx, "key", "value", 0).Err(); err != nil {
+		panic(err)
+	}
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val)
+	err = rdb.ForEachMaster(ctx, func(ctx context.Context, master *redis.Client) error {
+		fmt.Println(master, "is master")
+		return master.Ping(ctx).Err()
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = rdb.ForEachSlave(ctx, func(ctx context.Context, slave *redis.Client) error {
+		fmt.Println(slave, "is slave")
+		return slave.Ping(ctx).Err()
+	})
+	if err != nil {
+		panic(err)
+	}
+}
